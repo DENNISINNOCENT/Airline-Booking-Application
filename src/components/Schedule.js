efca684f6f9a0b0a1329310787d69ad74d2bcacf
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-07-31 21:54:38 +0300

    schedule.js

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,43 +1,43 @@
-// import React,{useEffect,useState} from 'react'
+import React,{useEffect,useState} from 'react'
 
 
-// function Schedule() {
-// const[scheduledata,setScheduleData]=useState([])
-//     useEffect(()=>{
-//         fetch(" http://localhost:8004/schedule")
-//         .then((response)=>response.json())
-//         .then((scheduledata)=>setScheduleData(scheduledata))
+function Schedule() {
+const[scheduledata,setScheduleData]=useState([])
+    useEffect(()=>{
+        fetch(" http://localhost:8004/schedule")
+        .then((response)=>response.json())
+        .then((scheduledata)=>setScheduleData(scheduledata))
 
-//     },[]);
+    },[]);
 
 
-//   return (
-//     <div>
-//      {scheduledata.map((scheduledatas)=>{
-//            return   <div key={scheduledatas.id}>
-//             <table border= "2 border black" className='tableData'>
-//              <tbody>
-//               <th>Date</th>
-//               <th>Depature</th>
-//                <th>Depature Time</th>
-//                <th>Destination</th>
-//                <th>Arrival Time</th>
-//              </tbody>
-//             <tbody>
-//               <td>{scheduledatas.date}</td>
-//               <td>{scheduledatas.depature}</td>
-//               <td>{scheduledatas.depatureTime}</td>
-//               <td>{scheduledatas.destination}</td>
-//               <td>{scheduledatas.arrivalTime}</td>
-//             </tbody>
+  return (
+    <div>
+     {scheduledata.map((scheduledatas)=>{
+           return   <div key={scheduledatas.id}>
+            <table border= "2 border black" className='tableData'>
+             <tbody>
+              <th>Date</th>
+              <th>Depature</th>
+               <th>Depature Time</th>
+               <th>Destination</th>
+               <th>Arrival Time</th>
+             </tbody>
+            <tbody>
+              <td>{scheduledatas.date}</td>
+              <td>{scheduledatas.depature}</td>
+              <td>{scheduledatas.depatureTime}</td>
+              <td>{scheduledatas.destination}</td>
+              <td>{scheduledatas.arrivalTime}</td>
+            </tbody>
              
 
                
-//               </table>
-//               </div>
-//      })}
-//     </div>
-//   )
-// }
+              </table>
+              </div>
+     })}
+    </div>
+  )
+}
 
-// export default Schedule
+export default Schedule
