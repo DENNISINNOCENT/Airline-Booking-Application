e66eca1ef763c5a48e5d4ea4c12605a52dfcf098
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-07-31 21:05:42 +0300

    footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,31 @@
-// import React from 'react'
-// import "../Footer.css"
+import React from 'react'
+import "../Footer.css"
 
-// function Footer() {
-//   return (
-//     <>
-//     <div className='footer'>
-//         <div>
-//       <h4> Contact</h4> 
-//       <p> <strong>Mobile No:</strong>254741991874</p>
-//       <p> <strong>Mobile No:</strong>254741991874</p>
-//       </div>
-//       <div className='social Media'>
-//         <h4>Social Media</h4>
-//        <p><i className="fa-brands fa-instagram-square">  _deinAirlines</i></p>
-//        <p><i className="fa-brands fa-facebook">  deinAirlines</i></p>
-//        <p><i className="fa-brands fa-linkedin">  deinAirlines</i></p>
+function Footer() {
+  return (
+    <>
+    <div className='footer'>
+        <div>
+      <h4> Contact</h4> 
+      <p> <strong>Mobile No:</strong>254741991874</p>
+      <p> <strong>Mobile No:</strong>254741991874</p>
+      </div>
+      <div className='social Media'>
+        <h4>Social Media</h4>
+       <p><i className="fa-brands fa-instagram-square">  _deinAirlines</i></p>
+       <p><i className="fa-brands fa-facebook">  deinAirlines</i></p>
+       <p><i className="fa-brands fa-linkedin">  deinAirlines</i></p>
 
-//       </div>
-//       <div className='email'>
-//         <h4>Email</h4>
-//          deinAirlines@gmail.com
-//       </div>
-//       <div className='location'></div>
+      </div>
+      <div className='email'>
+        <h4>Email</h4>
+         deinAirlines@gmail.com
+      </div>
+      <div className='location'></div>
 
-//   </div>
-//         </>
-//   )
-// }
+  </div>
+        </>
+  )
+}
 
-// export default Footer
+export default Footer
